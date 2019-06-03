87fc354f674cfbc3724de1b70a93a7fe0e99f567
Ngichu <brianngichu@gmail.com>
2019-06-03 15:41:26 +0300

    angular trial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './address-card/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'second-project';
+  user: User;
+  
+  constructor() {
+    this.user = new User();
+    this.user.name = 'Brian';
+    this.user.title = 'software engineer'
+    this.user.address = '1000 kenya avenue'
+    this.user.phone = [
+      '1323242',
+      '243435353'
+    ];
+  }
 }
